7d6aa0d41912116b8c1bda31b02383e97616e4a9
Muhammed shahas <Mshahas7886@gmail.com>
2021-03-19 01:43:38 +1300

    created folder structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { Button, Container} from 'react-bootstrap' ;
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h1 className="text-center">WorkTraker</h1>
+      <Button className="btn-warning ">
+       <h3 className="text-light">click here </h3> 
+      </Button>
+    </Container>
   );
 }
 
